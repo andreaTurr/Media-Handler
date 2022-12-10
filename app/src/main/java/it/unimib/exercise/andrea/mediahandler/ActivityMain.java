efa7a442c2863bdd02804e624e01e36754fbc3db
andreaTurr <a.turrini2@campus.unimib.it>
2022-12-10 22:35:58 +0100

    Working navigator

--- app/src/main/java/it/unimib/exercise/andrea/mediahandler/ActivityMain.java
+++ app/src/main/java/it/unimib/exercise/andrea/mediahandler/ActivityMain.java
@@ -1,8 +1,15 @@
 package it.unimib.exercise.andrea.mediahandler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -10,5 +17,20 @@ public class ActivityMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.top_appbar);
+        setSupportActionBar(toolbar);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().
+                findFragmentById(R.id.nav_host_fragment_main);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.fragment_main, R.id.fragment_audio,
+                R.id.fragment_video, R.id.fragment_settings).build();
+        // For the Toolbar
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        // For the BottomNavigationView
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
