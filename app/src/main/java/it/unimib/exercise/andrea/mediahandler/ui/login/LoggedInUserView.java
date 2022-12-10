8775f5d732e4c143a1803234573d871c5351be09
andreaTurr <a.turrini2@campus.unimib.it>
2022-12-10 16:59:26 +0100

    Added and Personalised automatically generated login fragment -added welcome_nav_graph -added main_nav_graph

---
+++ app/src/main/java/it/unimib/exercise/andrea/mediahandler/ui/login/LoggedInUserView.java
@@ -0,0 +1,17 @@
+package it.unimib.exercise.andrea.mediahandler.ui.login;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
